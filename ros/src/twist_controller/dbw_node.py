e78a6b1218d89b2cca14052f6cd7a8194d0a0038
Matthew Younkins <matthewyounkins@protonmail.com>
2018-01-25 18:05:46 -0800

    Updated dbw

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -4,6 +4,10 @@ import rospy
 from std_msgs.msg import Bool
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
 from geometry_msgs.msg import TwistStamped
+from lowpass import LowPassFilter
+
+from styx_msgs.msg import Lane
+
 import math
 
 from twist_controller import Controller
@@ -56,12 +60,40 @@ class DBWNode(object):
         # TODO: Create `TwistController` object
         # self.controller = TwistController(<Arguments you wish to provide>)
 
+        parms = {
+            'wheel_base'      : wheel_base,
+            'steer_ratio'     : steer_ratio,
+            'min_velocity'    : 0.,
+            'max_lat_accel'   : max_lat_accel,
+            'max_steer_angle' : max_steer_angle,
+            'decel_limit'     : decel_limit,
+            'accel_limit'     : accel_limit,
+            'deadband'        : brake_deadband
+        }
+
+        self.controller = Controller(**parms)
+
+        self.current_command  = None
+        self.current_velocity = None
+        self.dbw_enabled      = False
+
         # TODO: Subscribe to all the topics you need to
 
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.callback_twist_cmd)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.callback_current_velocity)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.callback_dbw_enabled)
+        rospy.Subscriber('/final_waypoints', Lane, self.final_waypoints_cb)
         self.loop()
 
+
+    def final_waypoints_cb(self, msg):  #should be OK
+        self.final_waypoints = msg
+        #rospy.logwarn(self.final_waypoints)
+        #rospy.logwarn('dbw_node ' + str(self.final_waypoints.waypoints[1].twist.twist.linear.z))
+        #rospy.logwarn(self.final_waypoints.waypoints[1].twist.twist.linear.x)
+
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(20) # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
@@ -72,6 +104,29 @@ class DBWNode(object):
             #                                                     <any other argument you need>)
             # if <dbw is enabled>:
             #   self.publish(throttle, brake, steer)
+            if (self.current_command is not None) and (self.current_velocity is not None):
+                #current velocity, target velocity, and target angle and pass into control
+                #rospy.logwarn('Setting Velocity')
+                linear_target  = self.current_command.twist.linear.x
+
+                #rospy.logwarn(str(linear_target) )
+                angular_target = self.current_command.twist.angular.z
+                linear_current = self.current_velocity.twist.linear.x
+                angular_current = self.current_velocity.twist.angular.z
+
+                throttle, brake, steering = self.controller.control(linear_target, angular_target, linear_current)
+                #rospy.logwarn(str(throttle))
+                brake = brake * 500
+                #rospy.logwarn(str(brake))
+                
+                
+                # publish the control commands if dbw is enabled
+                if self.dbw_enabled is True:
+
+                    self.publish(throttle, brake, steering)
+                else:
+                    rospy.logwarn('resetting controller')
+                    self.controller.reset()
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
@@ -91,6 +146,18 @@ class DBWNode(object):
         bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
+
+
+    def callback_twist_cmd(self, msg):
+        self.current_command = msg
+
+    def callback_current_velocity(self, msg):
+        self.current_velocity = msg
+
+    def callback_dbw_enabled(self, msg):
+        self.dbw_enabled = msg.data
+
+
 
 
 if __name__ == '__main__':
